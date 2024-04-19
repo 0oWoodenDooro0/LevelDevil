7d68a9f11cb304e0281d858a91252f8e84396f11
0oWoodenDooro0 <vincent031525@gmail.com>
2024-04-20 00:20:48 +0800

    resolve tag: performance

--- include/Animator.hpp
+++ include/Animator.hpp
@@ -12,18 +12,19 @@
 #include "Core/Drawable.hpp"
 #include <functional>
 
+typedef const std::function<void(std::shared_ptr<Core::Drawable>)> &set_drawable_function;
+
 class Animator {
 public:
     Animator() = default;
 
-    void SetAnimationStates(std::map<std::string, std::shared_ptr<Core::Drawable>> states);
+    void SetAnimationStates(std::map<std::string, std::shared_ptr<Core::Drawable>, std::less<>> states);
 
-    void UpdateAnimationState(const std::string &animation_state_name,
-                              const std::function<void(std::shared_ptr<Core::Drawable>)> &set_drawable);
+    void UpdateAnimationState(const std::string &animation_state_name, set_drawable_function set_drawable);
 
 private:
     std::string current_animation_state_;
-    std::map<std::string, std::shared_ptr<Core::Drawable>> animation_states_;
+    std::map<std::string, std::shared_ptr<Core::Drawable>, std::less<>> animation_states_;
 };
 
 
--- src/Animator.cpp
+++ src/Animator.cpp
@@ -6,12 +6,11 @@
 
 #include <utility>
 
-void Animator::SetAnimationStates(std::map<std::string, std::shared_ptr<Core::Drawable>> states) {
+void Animator::SetAnimationStates(std::map<std::string, std::shared_ptr<Core::Drawable>, std::less<>> states) {
     animation_states_ = std::move(states);
 }
 
-void Animator::UpdateAnimationState(const std::string &animation_state_name,
-                                    const std::function<void(std::shared_ptr<Core::Drawable>)> &set_drawable) {
+void Animator::UpdateAnimationState(const std::string &animation_state_name, set_drawable_function set_drawable) {
     if (current_animation_state_ == animation_state_name) return;
     current_animation_state_ = animation_state_name;
     set_drawable(animation_states_[animation_state_name]);
