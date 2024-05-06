5dbb0164f50077709e9e84cfab49dc5608892cda
楊宇詮 <t111590024@ntut.org.tw>
2024-05-06 11:26:03 +0800

    初步想法

--- src/MovableSprite.cpp
+++ src/MovableSprite.cpp
@@ -8,7 +8,7 @@
 MovableSprite::MovableSprite(const std::shared_ptr<Core::Drawable> &drawable, float z_index)
         : Sprite(drawable, z_index) {}
 
-void MovableSprite::Move(glm::vec2 target_position, float speed) {
+void MovableSprite::Move(glm::vec2 target_position, const std::shared_ptr<Character> &character, float speed) {
     auto vector = target_position - GetPosition();
     auto normalize = vector / hypot(vector.x, vector.y);
     if (hypot(vector.x, vector.y) != 0) {
@@ -16,8 +16,10 @@ void MovableSprite::Move(glm::vec2 target_position, float speed) {
         auto new_position = glm::vec2(int(position.x), int(position.y));
         if (abs(new_position.x) < abs(vector.x) || abs(new_position.y) < abs(vector.y)) {
             SetPosition(GetPosition() + new_position);
+            character->MoveWithSprite(std::make_shared<MovableSprite>(*this), new_position);
         } else {
             SetPosition(target_position);
+            character->MoveWithSprite(std::make_shared<MovableSprite>(*this), vector);
         }
     }
 }
